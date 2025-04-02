4b5bb61c902683e9a85b880496734153a002e08b
Clément REVERBEL <reverbel.cle@gmail.com>
2025-04-02 10:59:40 +0200

    Autocomplete silhouette

--- lib/pages/silhouette.dart
+++ lib/pages/silhouette.dart
@@ -47,23 +47,31 @@ class _silhouetteState extends State<silhouette> {
     });
   }
 
+  String gamemode(){
+    return _shape_mode? "Sans silhouette":"Silhouette";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child:
-            Column(
-                children:
-                [Text("Silhouette", style: TextStyle(fontSize: 40)),
-                  //! permet de vérifier si silhouette n'est pas null
-                  _shape_mode?
-                    Image.asset(_silhouette!.path_shape):
-                    Image.asset(_silhouette!.path_full_image),
-                  Switch(value: _shape_mode, onChanged: (bool value)=>setState((){
-                    _shape_mode = value;
-                  }))
-                ]
-            )
+      body: SingleChildScrollView(
+        child:
+          Column(
+            children:
+            [Text("Silhouette", style: TextStyle(fontSize: 40)),
+              //! permet de vérifier si silhouette n'est pas null
+              _shape_mode?
+                //Affiche l'image en fonction du mode de jeu
+                Image.asset(_silhouette!.path_shape):
+                Image.asset(_silhouette!.path_full_image),
+                SizedBox(height: 20),
+                Text("Mode de jeu : "+gamemode()),
+                Switch(value: _shape_mode, onChanged: (bool value)=>setState((){
+                _shape_mode = value;})),
+                AutoCompleteCharacter(),
+                SizedBox(height: 100)
+            ]
+          ),
         ),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
@@ -74,3 +82,35 @@ class _silhouetteState extends State<silhouette> {
         appBar: const boutonsMenu());
   }
 }
+
+//Cette classe permet de créer un textField autocomplétant personnalisé
+class AutoCompleteCharacter extends StatelessWidget {
+  const AutoCompleteCharacter({super.key});
+
+  //Ici se trouve les items qui peuvent être proposés
+  static const List<String> _kOptions = <String>['Alfred Woden',
+    'Anthony DeMarco',
+    'Max Payne',
+    'Mona Sax'
+    'Nicole Horne',
+    'Raul Passos',
+    'Tony DeMarco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Autocomplete<String>(
+      optionsBuilder: (TextEditingValue textEditingValue) {
+        if (textEditingValue.text == '') {
+          return const Iterable<String>.empty();
+        }
+        return _kOptions.where((String option) {
+          return option.contains(textEditingValue.text);
+        });
+      },
+      onSelected: (String selection) {
+        debugPrint('You just selected $selection');
+      },
+    );
+  }
+}
+
